8cf8391332cab0d57fe75b8a23f74623dfd53e09
MOON <sunpositive7@gmail.com>
2022-03-18 18:20:41 +0900

    Switch 라우팅
    if else의 규칙처럼 작은 단위부터 넓혀가는 방식으로 처리

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 import About from './pages/About';
 import Home from "./pages/Home";
 import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/profile/:id" component={Profile} />
-      <Route path="/about" exact component={About} />
+      <Switch>
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" exact component={About} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
