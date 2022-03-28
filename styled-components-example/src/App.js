64c0b020d8e43d362a454c1203c297c5a9173e6d
MOON <sunpositive7@gmail.com>
2022-03-28 22:45:53 +0900

    StyledButton.jsx에 코드를 넣은것 처럼 styled-components를 사용하여 css 처리 심화

--- styled-components-example/src/App.js
+++ styled-components-example/src/App.js
@@ -1,6 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import StyledButton from './components/StyledButton';
+import styled from 'styled-components';
+
+const PrimaryStyledButton = styled(StyledButton)`
+  background:palevioletred;
+  color:white;
+`
+
+const UppercaseButton = props => <button {...props} children={props.children.toUpperCase()} />;
+
+const MyButton = props => <button className={props.className} children={`MyButton ${props.children}`} />;
+
+const StyledMyButton = styled(MyButton)`
+  background: transparent;
+  border-radius:3px;
+  border:2px solid ${props => props.color || 'palevioletred'};
+  color:${props => props.color || 'palevioletred'};
+  margin:0 1em;
+  padding:0.25em 1em;
+  font-size:20px;
+
+  :hover{
+    border:2px solid red;
+  }
+  ::before{
+    content:"@";
+  }
+`
 
 function App() {
   return (
@@ -9,6 +36,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <StyledButton>버튼</StyledButton>
+          <StyledButton primary>버튼</StyledButton>
+          <PrimaryStyledButton>버튼</PrimaryStyledButton>
+          <StyledButton as="a" href="/">버튼</StyledButton>
+          <StyledButton as={UppercaseButton}>button</StyledButton>
+          <StyledMyButton>button</StyledMyButton>
+          <StyledMyButton color="green">button</StyledMyButton>
         </p>
       </header>
     </div>
