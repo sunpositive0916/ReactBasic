e558152e08b02a0728f19c6ff0ddc30eb8796bbb
MOON <sunpositive7@gmail.com>
2022-03-28 22:31:45 +0900

    StyledButton.jsx에 코드를 넣은것 처럼 styled-components를 사용하여 css 처리 가능

---
+++ styled-components-example/src/components/StyledButton.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+background: transparent;
+border-radius:3px;
+border:2px solid palevioletred;
+color:palevioletred;
+margin:0 1em;
+padding:0.25em 1em;
+font-size:20px;
+`;
+
+export default StyledButton;
